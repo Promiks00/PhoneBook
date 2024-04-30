40dea3f924a4cdf6ef8dcda8b03d92621722b15e
Dmirty <prodemiks@mail.ru>
2024-04-30 12:42:11 +0300

    added variables and empty methods of program

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,7 +1,25 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class PhoneBook {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        HashMap<String, List<String>> phoneBook = new HashMap<>();
+        String name, phoneNumber;
+
+        while(true){
+
+        }
+
+        void addContact(HashMap<String, List<String>> phoneBook, String name, String phoneNumber){
+
+        }
+
+        void printPhoneBook(HashMap<String, List<String>> phoneBook){
+
+        }
     }
 }
